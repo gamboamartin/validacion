07c8cdc46bb87a4e112286b7a0883b63ced777e0
mgamboa <gamboa.martin@gmail.com>
2023-01-16 16:37:24 -0600

    v1.4.0 Se integra ut init cod 0 numers

--- src/validacion.php
+++ src/validacion.php
@@ -323,10 +323,14 @@ class validacion {
     /**
      * Integra una expresion regular del 0 al 9 repitiendo los numeros n veces = a la longittud
      * @param int $longitud Longitud de la cadena permitida de numeros
-     * @return string
+     * @return string|array
+     * @version 1.4.0
      */
-    private function init_cod_int_0_n_numbers(int $longitud): string
+    private function init_cod_int_0_n_numbers(int $longitud): string|array
     {
+        if($longitud<=0){
+            return  $this->error->error(mensaje: 'Error longitud debe ser mayor a 0',data: $longitud);
+        }
         $key = 'cod_int_0_'.$longitud.'_numbers';
         $this->patterns[$key] = '/^[0-9]{'.$longitud.'}$/';
         return $this->patterns[$key];
